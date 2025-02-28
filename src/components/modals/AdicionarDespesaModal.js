18253f29250bff1c9dfaf39b8e3c603dd48ecbf2
Anhaia09 <leticia.anhaia09@gmail.com>
2025-02-28 14:42:11 -0300

    :sparkles:	feat: added bars in the date field and added function for inserting current date

--- src/components/modals/AdicionarDespesaModal.js
+++ src/components/modals/AdicionarDespesaModal.js
@@ -1,13 +1,24 @@
-import React, { useState, useContext } from 'react';
-import { SaldoContext } from '../../contexts/SaldoContext';
-import { Modal, View, TextInput, TouchableOpacity, Text, Alert } from 'react-native';
-import { validarValor } from '../../utils/validation'; // Importa a função de validação
+import React, {useState, useContext} from 'react';
+import {SaldoContext} from '../../contexts/SaldoContext';
+import {
+  Modal,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  Alert,
+} from 'react-native';
+import {validarValor} from '../../utils/validation'; // Importa a função de validação
 import styles from './AdicionarDespesaModal.styles'; // Importa os estilos
-import { validarData } from '../../utils/validateData'; // Importa a função de validação
+import {validarData} from '../../utils/validateData'; // Importa a função de validação
 import storage from '../../services/storage'; // Importa o módulo de armazenamento
 import uuid from 'react-native-uuid'; // Importa a biblioteca para gerar IDs únicos
 
-const AdicionarDespesaModal = ({ modalVisible, setModalVisible, setDespesas }) => {
+const AdicionarDespesaModal = ({
+  modalVisible,
+  setModalVisible,
+  setDespesas,
+}) => {
   // Estados para armazenar os valores dos campos do formulário de nova despesa
   const [novaDescricao, setNovaDescricao] = useState('');
   const [novoValor, setNovoValor] = useState('');
@@ -16,48 +27,81 @@ const AdicionarDespesaModal = ({ modalVisible, setModalVisible, setDespesas }) =
   const [novoMetodoPagamento, setNovoMetodoPagamento] = useState('');
   const {saldo, setSaldo} = useContext(SaldoContext);
 
-    const adicionarDespesa = async novaDespesa => {
-      try {
-        // Gerando um ID único para a nova despesa
-        const despesaComId = {
-          id: uuid.v4(), // Adiciona um ID único
-          ...novaDespesa,
-        };
-  
-        // Obtendo despesas salvas
-        const existingExpenses = storage.getString('expenses');
-        const parsedExpenses = existingExpenses
-          ? JSON.parse(existingExpenses)
-          : [];
-  
-        // Adicionando nova despesa com ID
-        const updatedExpenses = [...parsedExpenses, despesaComId];
-  
-        // Salvando no MMKV
-        storage.set('expenses', JSON.stringify(updatedExpenses));
-  
-        // Atualizando o estado
-        setDespesas(updatedExpenses);
-        console.log('updatedExpenses:', updatedExpenses);
-  
-        // Atualizando saldo
-        const novoSaldo = saldo - novaDespesa.valor;
-        setSaldo(novoSaldo);
-      } catch (error) {
-        console.error('Erro ao acessar o MMKV:', error.message);
-      }
-    };
+  // Função para formatar a data no formato DD/MM/YYYY enquanto o usuário digita
+  const formatarData = input => {
+    let valor = input.replace(/\D/g, ''); // Remove tudo que não é número
+    if (valor.length > 2) {
+      valor = valor.replace(/^(\d{2})(\d)/g, '$1/$2'); // Adiciona a primeira barra após o dia
+    }
+    if (valor.length > 5) {
+      valor = valor.replace(/^(\d{2})\/(\d{2})(\d)/g, '$1/$2/$3'); // Adiciona a segunda barra após o mês
+    }
+    if (valor.length > 10) {
+      valor = valor.substring(0, 10); // Limita o campo a 10 caracteres (DD/MM/YYYY)
+    }
+    return valor;
+  };
+
+  const adicionarDespesa = async novaDespesa => {
+    try {
+      // Gerando um ID único para a nova despesa
+      const despesaComId = {
+        id: uuid.v4(), // Adiciona um ID único
+        ...novaDespesa,
+      };
+      // Obtendo despesas salvas
+      const existingExpenses = storage.getString('expenses');
+      const parsedExpenses = existingExpenses
+        ? JSON.parse(existingExpenses)
+        : [];
+
+      // Adicionando nova despesa com ID
+      const updatedExpenses = [...parsedExpenses, despesaComId];
+
+      // Salvando no MMKV
+      storage.set('expenses', JSON.stringify(updatedExpenses));
+
+      // Atualizando o estado
+      setDespesas(updatedExpenses);
+      console.log('updatedExpenses:', updatedExpenses);
+
+      // Atualizando saldo
+      const novoSaldo = saldo - novaDespesa.valor;
+      setSaldo(novoSaldo);
+    } catch (error) {
+      console.error('Erro ao acessar o MMKV:', error.message);
+    }
+  };
 
   const handleAdicionarDespesa = () => {
-
     // Valida a data digitada
     if (!validarData(novaData)) {
-      Alert.alert('Erro', 'Por favor, insira uma data válida no formato DD/MM/YYYY.');
-      setNovaData('');  // Limpa o campo de data em caso de erro
+      setNovaData(''); // Limpa o campo de data em caso de erro
       return;
     }
+
+    // Verifica se a data é válida e não é futura
+    const [dia, mes, ano] = novaData.split('/').map(Number);
+    const dataDigitada = new Date(ano, mes - 1, dia);
+    const dataAtual = new Date();
+
+    if (
+      dataDigitada.getFullYear() !== ano ||
+      dataDigitada.getMonth() + 1 !== mes ||
+      dataDigitada.getDate() !== dia
+    ) {
+      setNovaData('');
+      return;
+    }
+
     // Verifica se todos os campos foram preenchidos
-    if (!novaDescricao || !novoValor || !novaData || !novoEstabelecimento || !novoMetodoPagamento) {
+    if (
+      !novaDescricao ||
+      !novoValor ||
+      !novaData ||
+      !novoEstabelecimento ||
+      !novoMetodoPagamento
+    ) {
       Alert.alert('Erro', 'Por favor, preencha todos os campos.');
       return;
     }
@@ -65,7 +109,7 @@ const AdicionarDespesaModal = ({ modalVisible, setModalVisible, setDespesas }) =
     // Valida o valor digitado
     if (!validarValor(novoValor)) {
       Alert.alert('Erro', 'Por favor, insira um valor válido maior que zero.');
-      setNovoValor('');  // Limpa o campo de valor em caso de erro
+      setNovoValor(''); // Limpa o campo de valor em caso de erro
       return;
     }
 
@@ -111,8 +155,23 @@ const AdicionarDespesaModal = ({ modalVisible, setModalVisible, setDespesas }) =
             style={styles.input}
             placeholder="Data"
             placeholderTextColor="#7F8C8D"
+            keyboardType="numeric"
             value={novaData}
-            onChangeText={setNovaData}
+            onChangeText={text => {
+              const formattedText = formatarData(text); // Formata a data
+              const [dia, mes, ano] = formattedText.split('/').map(Number);
+              const dataDigitada = new Date(ano, mes - 1, dia);
+              const dataAtual = new Date();
+
+              // Verifica se a data é futura
+              if (dataDigitada > dataAtual) {
+                Alert.alert('Erro', 'Não é permitido inserir uma data futura.');
+                return;
+              }
+
+              setNovaData(formattedText); // Atualiza o estado da data
+            }}
+            maxLength={10} // Limita o campo a 10 caracteres
           />
           <TextInput
             style={styles.input}
