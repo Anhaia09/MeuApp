881a965c3b07ed4dc95bfb67a7bd30ec4253a5ba
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2025-03-21 12:46:52 -0300

    :wastebasket: remove: SaldoContext file and related code

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Routes from './src/Routes'; // Importando as rotas
-import { SaldoProvider } from './src/contexts/SaldoContext'; // Importando o contexto de saldo
+
 
 const App = () => {
   return (
-    <SaldoProvider>
-      <Routes /> 
-    </SaldoProvider> 
+      <Routes />
   );
 };
 
